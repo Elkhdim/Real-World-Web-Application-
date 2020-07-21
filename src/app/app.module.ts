335ff262e3c4d52b3b50af172c22c74b20176540
elkhdim <abdelilahelkhdim1@gmail.com>
2020-07-21 13:46:17 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ArticleListComponent } from './component/article-list/article-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
